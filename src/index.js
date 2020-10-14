17f91d8954bed2b453cb7f4cf03809f91d128251
AibarBaltykhan <https://github.com/AibarBaltykhan>
2020-10-14 16:56:23 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length <= 1) {
+        return false;
+    }
+    let matchingOpeningBracket, ch;
+    let stack = [];
+    let openingBrackets = [];
+    let closingBrackets = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        openingBrackets.push(bracketsConfig[i][0]);
+        closingBrackets.push(bracketsConfig[i][1]);
+    }
+    for (let i = 0; i < str.length; i++) {
+        ch = str[i];
+        if (closingBrackets.indexOf(ch) > -1) {
+            matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(ch)];
+
+            if (matchingOpeningBracket != ch) {
+                if (stack.length == 0 || (stack.pop() != matchingOpeningBracket)) {
+                    return false;
+                }
+            } else {
+                if (stack[stack.length-1] === matchingOpeningBracket) {
+                    stack.pop();
+                } else {
+                    stack.push(ch);
+                }
+            }
+        } else {
+            stack.push(ch);
+        }
+    }
+    if (stack.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
